7df27cbdd2bb30a036fd23cc8a07b383f58d1f1c
Kraker <ibrahimaelbana@gmail.com>
2021-08-27 14:35:06 +0200

    fuckeed up

--- scripts/main.js
+++ scripts/main.js
@@ -24,154 +24,16 @@ slider.forEach((slider) => {
   });
 });
 
-// when you make them links use this
 
-// const slider = document.querySelector(".wrapper");
-//       const preventClick = (e) => {
-//         e.preventDefault();
-//         e.stopImmediatePropagation();
-//       }
-//       let isDown = false;
-//       var isDragged = false;
-//       let startX;
-//       let scrollLeft;
+let items = document.querySelectorAll('.accordion-body--main .col');
+let mainData = document.querySelector('.accordion-body--main')
+let clickedData = document.querySelectorAll('#headsetCollapse .accordion-body--clicked')
 
-//       slider.addEventListener("mousedown", e => {
-//         isDown = true;
-//         slider.classList.add("active");
-//         startX = e.pageX - slider.offsetLeft;
-//         scrollLeft = slider.scrollLeft;
-//       });
+console.log(mainData)
 
-//       slider.addEventListener("mouseleave", () => {
-//         isDown = false;
-//         slider.classList.remove("active");
-//       });
-
-//       slider.addEventListener("mouseup", e => {
-//         isDown = false;
-//         const elements = document.getElementsByClassName("book");
-//         if(isDragged){
-//             for(let i = 0; i<elements.length; i++){
-//                   elements[i].addEventListener("click", preventClick);
-//             }
-//         }else{
-//             for(let i = 0; i<elements.length; i++){
-//                   elements[i].removeEventListener("click", preventClick);
-//             }
-//         }
-//         slider.classList.remove("active");
-//         isDragged = false;
-//       });
-
-//       slider.addEventListener("mousemove", e => {
-//         if (!isDown) return;
-//         isDragged =  true;
-//         e.preventDefault();
-//         const x = e.pageX - slider.offsetLeft;
-//         const walk = (x - startX) * 2;
-//         slider.scrollLeft = scrollLeft - walk;
-//       });
-
-//       document.getElementsByClassName("book").ondragstart = function() {
-//         console.log("Drag start");
-//       };
-
-// function to split the data into parts 8 or less each part
-
-
-// const bedBaseData = [
-//   {
-//     title: "a",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "b",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "c",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "d",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "e",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "f",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "g",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "h",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "i",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "j",
-//     img: "./assets/Image.png",
-//   },
-//   {
-//     title: "k",
-//     img: "./assets/Image.png",
-//   },
-// ];
-
-// const divide = (data) => {
-//   const n = 8; //tweak this to add more items per line
-//   const result = new Array(Math.ceil(data.length / n))
-//     .fill()
-//     .map((_) => data.splice(0, n));
-//   return result;
-// };
-
-// let bedBaseCollapse = document.querySelector(
-//   "#bedBaseCollapse .accordion-body"
-// );
-
-// const renderRow = (data, element) => {
-//   console.log(data);
-//   data.map((item, idx) => {
-//     console.log(item)
-//     let child = renderItem(item);
-//     console.log(child)
-//     let html = `
-//       <div class="row row-cols-3 row-cols-md-4 flex-nowrap overflow-scroll">
-//         ${child}
-//       </div>
-//       `;
-//       console.log(element)
-//       element.insertAdjacentHTML(
-//         "beforeend",
-//         html
-//       );      
-//   });
-// };
-
-// const renderItem = (data) => {
-//   let html = "";
-//   data.forEach((item, idx) => {
-//     html += `
-//     <div class="col">
-//       <div class="img-container">
-//         <img src=${item.img} alt="customize" class="img-fluid" />
-//       </div>
-//       <p>${item.title}</p>
-//     </div>
-//     `;
-//   });
-//   return html;
-// };
-
-
-// renderRow(divide(bedBaseData), bedBaseCollapse)
+items.forEach((item, idx) => {
+  item.addEventListener('click', () => {
+    mainData.classList.add('d-none')
+    clickedData.forEach(item => item.classList.remove('d-none'))
+  })
+})
