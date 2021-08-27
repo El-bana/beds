f681eb54e469aaf316f9b8aaca29f7d03ed5aa3e
Kraker <ibrahimaelbana@gmail.com>
2021-08-27 13:31:28 +0200

    dynamic render finished

--- scripts/main.js
+++ scripts/main.js
@@ -76,3 +76,100 @@ slider.forEach((slider) => {
 //       document.getElementsByClassName("book").ondragstart = function() {
 //         console.log("Drag start");
 //       };
+
+// function to split the data into parts 8 or less each part
+const bedBaseData = [
+  {
+    title: "a",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "b",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "c",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "d",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "e",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "f",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "g",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "h",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "i",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "j",
+    img: "./assets/Image.png",
+  },
+  {
+    title: "k",
+    img: "./assets/Image.png",
+  },
+];
+
+const divide = (data) => {
+  const n = 8; //tweak this to add more items per line
+  const result = new Array(Math.ceil(data.length / n))
+    .fill()
+    .map((_) => data.splice(0, n));
+  return result;
+};
+
+let bedBaseCollapse = document.querySelector(
+  "#bedBaseCollapse .accordion-body"
+);
+
+const renderRow = (data, element) => {
+  console.log(data);
+  data.map((item, idx) => {
+    console.log(item)
+    let child = renderItem(item);
+    console.log(child)
+    let html = `
+      <div class="row row-cols-3 row-cols-md-4 flex-nowrap overflow-scroll">
+        ${child}
+      </div>
+      `;
+      console.log(element)
+      element.insertAdjacentHTML(
+        "beforeend",
+        html
+      );      
+  });
+};
+
+const renderItem = (data) => {
+  let html = "";
+  data.forEach((item, idx) => {
+    html += `
+    <div class="col">
+      <div class="img-container">
+        <img src=${item.img} alt="customize" class="img-fluid" />
+      </div>
+      <p>${item.title}</p>
+    </div>
+    `;
+  });
+  return html;
+};
+
+
+renderRow(divide(bedBaseData), bedBaseCollapse)
