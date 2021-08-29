b2e56c7012a2dc970f38e75719b966608eb1edc9
Kraker <ibrahimaelbana@gmail.com>
2021-08-29 16:41:20 +0200

    heeeeh

--- scripts/main.js
+++ scripts/main.js
@@ -24,33 +24,55 @@ slider.forEach((slider) => {
   });
 });
 
-
-
 // accordion functionality
 
 const show = (item) => {
-  item.classList.remove('d-none')
-}
+  item.classList.remove("d-none");
+};
 
 const hide = (item) => {
-  item.classList.add('d-none')
-}
+  item.classList.add("d-none");
+};
 
-
-
-let items = document.querySelectorAll('.accordion-body--main .col');
-let mainData = document.querySelectorAll('.accordion-body--main')
-let clickedData = document.querySelectorAll('.accordion-body--clicked')
-
-
+let items = document.querySelectorAll(".accordion-body--main .col");
+let mainData = document.querySelectorAll(".accordion-body--main");
+let clickedData = document.querySelectorAll(".accordion-body--clicked");
 
 items.forEach((item) => {
-  item.addEventListener('click', (e) => {
-    
-    show(document.querySelector(item.getAttribute('data-show-target')))
-    hide(document.querySelector(item.getAttribute('data-hide-target')))
-  })
-})
+  item.addEventListener("click", (e) => {
+    show(document.querySelector(item.getAttribute("data-show-target")));
+    hide(document.querySelector(item.getAttribute("data-hide-target")));
+  });
+});
 
 // for each maindata check data target
 // the items clicked get the id = data targets clicked and remove d-none
+
+const accordionHeight = () => {
+  if (screen.width < 1280) {
+    let container = document.querySelector(".container.position-relative");
+    let img = document.querySelector(".left-side-image");
+    let buttons = document.querySelector(".buttons-row");
+    let accordion = document.querySelector(".accordion-menu");
+
+    let h =
+      container.offsetHeight -
+      (img.offsetHeight +
+        buttons.offsetHeight +
+        parseInt(getComputedStyle(accordion).marginTop));
+
+    accordion.style.height = `${h}px`;
+  } else {
+    let img = document.querySelector(".left-side-image");
+    let accordion = document.querySelector(".accordion-menu");
+
+    let h =
+      img.offsetHeight +
+      (parseInt(getComputedStyle(img).marginTop) -
+        parseInt(getComputedStyle(accordion).marginTop));
+
+    accordion.style.height = `${h}px`;
+  }
+};
+
+accordionHeight()
